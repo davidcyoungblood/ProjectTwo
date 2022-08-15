6c9a0255cec8a1744b9627dd5bc0f6926bf60922
David <dyoungblood@skillstorm.com>
2022-08-15 09:44:16 -0400

    8-15 bug fix

--- React/react-project-two/src/components/index.js
+++ React/react-project-two/src/components/index.js
@@ -2,10 +2,3 @@ export * from "./Logo";
 export * from "./Footer";
 export * from "./Copyright"; //template
 export * from "./ThunderstormIcon"; //template
-export * from "./Pricing"; //template
-// export * from "./cardData/ServicePlan";
-// export * from "./ServicePlanList";
-// export * from "./cardData/Status";
-// export * from "./StatusList";
-// export * from "./cardData/Interval";
-// export * from "./IntervalList";
